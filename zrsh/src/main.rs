f482cd6df9ecdf3e3908bd3da0b1f4cc51c1b0bf
ueki5 <ueki005@gmail.com>
2024-05-16 23:41:13 +0900

    1

--- zrsh/src/main.rs
+++ zrsh/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod helper;
+mod shell;
+
+use helper::DynError;
+
+const HISTORY_FILE: &str = ".zerosh_history";
+
+fn main() -> Result<(), DynError> {
+    let mut logfile = HISTORY_FILE;
+    let mut home = dirs::home_dir();
+    if let Some(h) = &mut home {
+        h.push(HISTORY_FILE);
+        logfile = h.to_str().unwrap_or(HISTORY_FILE);
+    }
+
+    let sh = shell::Shell::new(logfile);
+    sh.run()?;
+
+    Ok(())
 }
