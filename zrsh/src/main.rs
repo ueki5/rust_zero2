80aa2fc7ef628c4751658bedae30c19f826e23e9
ueki5 <ueki005@gmail.com>
2024-05-30 01:50:44 +0900

    テスト

--- zrsh/src/main.rs
+++ zrsh/src/main.rs
@@ -1,3 +1,27 @@
+// 確認用
+use nix::{
+    libc,
+    sys::{
+        signal::{killpg, signal, SigHandler, Signal},
+        wait::{waitpid, WaitPidFlag, WaitStatus},
+    },
+    unistd::{self, dup2, execvp, fork, pipe, setpgid, tcgetpgrp, tcsetpgrp, ForkResult, Pid},
+};
+use std::{
+    collections::{BTreeMap, HashMap, HashSet},
+    ffi::CString,
+    fs::File,
+    mem::replace,
+    path::Path,
+    path::PathBuf,
+    process::exit,
+    sync::mpsc::{channel, sync_channel, Receiver, Sender, SyncSender},
+    thread,
+    io,
+};
+use std::os::fd::AsFd;
+use std::os::fd::AsRawFd;
+// 確認用
 mod helper;
 mod shell;
 
@@ -6,6 +30,13 @@ use helper::DynError;
 const HISTORY_FILE: &str = ".zerosh_history";
 
 fn main() -> Result<(), DynError> {
+    // 確認用
+    let stdin = io::stdin(); // We get `Stdin` here.
+    let raw_fd = stdin.as_fd().as_raw_fd();
+    let res = unsafe { libc::tcgetpgrp(raw_fd) };
+    let res2 = unsafe { libc::tcgetpgrp(libc::STDOUT_FILENO) };
+    // 確認用
+
     let mut logfile = HISTORY_FILE;
     let mut home = dirs::home_dir();
     if let Some(h) = &mut home {
