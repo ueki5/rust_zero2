cdfff16436be4dbb60ef7d2bf96f9772ae33c568
ueki5 <ueki005@gmail.com>
2024-06-21 01:01:22 +0900

    1

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,4 +1,10 @@
 use nix::unistd::pipe;
+use std::fs::File;
+#[cfg(unix)]
+use std::os::unix::io::{AsFd, BorrowedFd, OwnedFd};
+#[cfg(windows)]
+use std::os::windows::io::{AsHandle, BorrowedHandle, OwnedHandle};
+
 fn main() {
     let mut input = None;
     let mut output = None;
@@ -6,4 +12,17 @@ fn main() {
     let p = pipe().unwrap();
     input = Some(p.0);
     output = Some(p.1);
+    println!("!!!");
+
+    let f = File::open("hoge.txt").unwrap();
+
+    // 生ハンドルとして借用
+    {
+        let raw: BorrowedFd = f.as_fd();
+    }
+
+    // 所有権を持つ生ハンドルに変換
+    let raw: OwnedFd = f.into();
+
+    // ファイルはOwnedFd/OwnedHandleによって正常に閉じられる
 }
